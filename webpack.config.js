ccc82f27cea1eab9041d51988e92796a11199dbe
limi58 <limi58@foxmail.com>
2015-10-11 23:39:08 +0800

    update the config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,47 @@
-// npm install --save react 
+// npm install --save react react-router
 // npm install -D react-hot-loader babel-loader style-loader css-loader less-loader webpack webpack-dev-server
 
 var path = require('path');
 var webpack = require('webpack');
 
+
 module.exports = {
+    devtool: false,
+    context: __dirname,
     entry: [
       'webpack-dev-server/client?http://localhost:3000',
       'webpack/hot/only-dev-server',
-      './entry'
+      './src/js/entry.jsx'
     ],
     output: {
-        path: __dirname,
-        filename: "bundle.js"
+        publicPath: 'http://localhost:3000/dist/images',
+        path: './dist',
+        filename: "/js/bundlewp.js"
     },
     module: {
       loaders: [
-        { test: /\.css$/, loader: "style!css" },
-        {
-          test: /\.js?$/,
-          loaders: ['react-hot', 'babel'],
+        { 
+          test: /\.jsx?$/, 
+          loader: 'babel-loader',
           exclude: /node_modules/,
           include: __dirname
-        }
+        },
+        // {
+        //   test: /\.jsx?$/,
+        //   loaders: ['react-hot', 'babel'],
+        //   exclude: /node_modules/,
+        //   include: __dirname
+        // },
+        {
+            test: /\.(otf|eot|svg|ttf|woff)/,
+            loader: 'url-loader?limit=68192'
+        },
+        { test: /\.less$/, loader: "style!css!less" },
+        { test: /\.css$/, loader: "style!css" }
       ]
     },
     plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NoErrorsPlugin()
+      // new webpack.HotModuleReplacementPlugin(),
+      // new webpack.NoErrorsPlugin()
     ]
 };
