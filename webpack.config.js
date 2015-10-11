b15aeb37653444e1551f9825ccea8b54e926b538
limi58 <limi58@foxmail.com>
2015-10-11 23:44:39 +0800

    update the config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
 // npm install --save react react-router
 // npm install -D react-hot-loader babel-loader style-loader css-loader less-loader webpack webpack-dev-server
 
-var path = require('path');
 var webpack = require('webpack');
 
 
 module.exports = {
     devtool: false,
+    // entry 和 output 的绝对位置
     context: __dirname,
+    //页面入口文件配置
     entry: [
       'webpack-dev-server/client?http://localhost:3000',
       'webpack/hot/only-dev-server',
       './src/js/entry.jsx'
     ],
     output: {
+        // 资源路径的前缀而已，便于更改cdn
         publicPath: 'http://localhost:3000/dist/images',
+        // 所有 output 输出的绝对位置，js、jpg 等等
         path: './dist',
-        filename: "/js/bundlewp.js"
+        filename: "/js/bundle.js"
     },
     module: {
       loaders: [
@@ -40,6 +43,11 @@ module.exports = {
         { test: /\.css$/, loader: "style!css" }
       ]
     },
+    // require 可以免掉后缀名
+    resolve: {
+        extensions: ['', '.js', '.json', '.scss'],
+    },
+    // 插件
     plugins: [
       // new webpack.HotModuleReplacementPlugin(),
       // new webpack.NoErrorsPlugin()
