c9e9e4cd96eea591ab8040a52cb86849be57cdc5
limi58 <limi58@foxmail.com>
2015-11-04 19:01:54 +0800

    optimize and add package script

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # webpackConfig
-some webpack config js
+some webpack and gulpfile config js
+
+# package scripts
+```js
+"scripts": {
+  "test": "echo \"Error: no test specified\" && exit 1",
+  "start": "node server.js",
+  "build": "set NODE_ENV=dev&&webpack --progress --profile --colors",
+  "dist": "set NODE_ENV=production&&webpack -p --progress --profile --colors"
+},
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 
 var webpack = require('webpack')
 var path = require('path')
+var isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
-    // devtool: false,
+    debug: isProduction ? false : true,
+    devtool: false,
     // entry 和 output 的绝对位置
     // context: __dirname,
     //页面入口文件配置
-    entry: [
+    entry: isProduction ? ['./src/js/entry.jsx'] : [
       'webpack-dev-server/client?http://localhost:3000/',
       'webpack/hot/only-dev-server',
       './src/js/entry.jsx'
@@ -21,21 +23,14 @@ module.exports = {
     },
     module: {
       loaders: [
-        // { 
-        //   test: /\.jsx?$/, 
-        //   loader: 'babel-loader',
-        //   exclude: /node_modules/,
-        //   include: __dirname
-        // },
         {
           test: /\.jsx?$/,
-          loaders: ['react-hot', 'babel'],
-          exclude: /node_modules/,
-          include: __dirname
+          loaders: isProduction ? ['babel'] : ['react-hot', 'babel'],
+          exclude: /node_modules/
         },
         {
-            test: /\.(otf|eot|svg|ttf|woff)/,
-            loader: 'url-loader?limit=68192'
+          test: /\.(otf|eot|svg|ttf|woff)/,
+          loader: 'url-loader?limit=268192'
         },
         { test: /\.scss$/, loader: "style!css!sass" },
         { test: /\.css$/, loader: "style!css" }
@@ -46,14 +41,20 @@ module.exports = {
     resolve: {
         extensions: ['', '.js', '.json', '.scss', '.jsx'],
         alias: {
+          'panel': path.join(__dirname, 'src/js/components/common/panel/panel.jsx'),
+          'modal': path.join(__dirname, 'src/js/components/common/modal.jsx'),
+          'loader': path.join(__dirname, 'src/js/components/common/loader.jsx'),
+          'uploadGroup': path.join(__dirname, 'src/js/components/common/uploadGroup.jsx'),
+
           'session': path.join(__dirname, 'src/js/servies/session.js'),
           'errorCatch': path.join(__dirname, 'src/js/servies/errorCatch.js'),
           'ajax': path.join(__dirname, 'src/js/servies/ajax.js'),
-          'panel': path.join(__dirname, 'src/js/components/common/panel/panel.jsx')
+          'publicStore': path.join(__dirname, 'src/js/stores/public/publicStore.js'),
+          'publicAction': path.join(__dirname, 'src/js/actions/public/publicAction.js'),
         }
     },
     // 插件
-    plugins: [
+    plugins: isProduction ? '' : [
       new webpack.HotModuleReplacementPlugin(),
     ]
-};
+}
