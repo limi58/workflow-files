d3851d600e33f74cc3f7b4d944ef8354ec96f470
limi58 <limi58@foxmail.com>
2016-06-15 13:58:50 +0800

    add umd travis etc.

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # workflow config
 Some webpack and gulpfile config files
 
-package.json
+# package.json scripts
 ```js
 "scripts": {
-  "start": "node server.js",
-  "dist": "set NODE_ENV=production&webpack -p --define process.env.NODE_ENV='production' --progress --colors"
+  "start": "set NODE_ENV=development&&webpack --watch ./src/libname.js ./dist/libname.js",
+  "build": "npm run dev&&npm run min",
+  "dev": "set NODE_ENV=development&&webpack ./src/libname.js ./dist/libname.js",
+  "min": "set NODE_ENV=production&&webpack -p src/libname.js dist/libname.min.js",
+  "test": "node ./test/test.js"
 }
+```
+
+# install 
+```
+npm --registry=https://registry.npm.taobao.org install --save-dev webpack babel-core babel-preset-es2015 babel-loader url-loader style-loader css-loader sass-loader webpack-dev-server
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,50 @@
 const webpack = require('webpack')
 const path = require('path')
-const isProduction = process.env.NODE_ENV === 'production'
+const DEBUG = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-    debug: isProduction ? false : true,
-    devtool: isProduction ? '' : 'source-map',
-    //页面入口文件配置
-    entry: isProduction ? ['./src/js/entry.js'] : [
-      'webpack-dev-server/client?http://localhost:3000/',
-      'webpack/hot/only-dev-server',
-      './src/js/entry.js'
-    ],
-    output: {
-        // 资源路径的前缀而已，便于更改cdn
-        publicPath: 'http://localhost:3000/',
-        // 所有 output 输出的绝对位置，js、jpg 等等
-        path: path.resolve('./dist/'),
-        filename: "js/bundle.js"
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.jsx?$/,
-          loaders: isProduction ? ['babel'] : ['react-hot', 'babel'],
-          exclude: /node_modules/
-        },
-        {
-          test: /\.(otf|eot|svg|ttf|woff)/,
-          loader: 'url-loader?limit=268192'
-        },
-        { test: /\.scss$/, loader: "style!css!sass" },
-        { test: /\.css$/, loader: "style!css" }
-      ]
-    },
-    // require 可以免掉后缀名
-    resolve: {
-      extensions: ['', '.js', '.json', '.scss', '.jsx'],
-      // alias 可以设置别名
-      alias: {
-        'config': path.join(__dirname, 'config.js')
-      }
-    },
-    // 插件
-    plugins: isProduction ? [] : [
-      new webpack.HotModuleReplacementPlugin()
+  debug: DEBUG,
+  devtool: DEBUG ? 'source-map' : '',
+  // entry: DEBUG ? [
+  //   // 'webpack-dev-server/client?http://localhost:3000/',
+  //   // 'webpack/hot/only-dev-server',
+  //   './src/libname.js'
+  // ] : './src/libname.js',
+  output: {
+    library: 'libname',
+    libraryTarget: 'umd',
+    // 资源路径的前缀而已，便于更改cdn
+    // publicPath: 'http://localhost:3000/',
+    // // 所有 output 输出的绝对位置，js、jpg 等等
+    // path: path.join(__dirname, 'dist'),
+    // filename: "libname.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: DEBUG ? ['react-hot', 'babel'] : ['babel'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(otf|eot|svg|ttf|woff)/,
+        loader: 'url-loader?limit=268192'
+      },
+      { test: /\.scss$/, loader: "style!css!sass" },
+      { test: /\.css$/, loader: "style!css" }
     ]
+  },
+  // require 可以免掉后缀名
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss', '.jsx'],
+    // alias 设置别名
+    alias: {
+      'config': path.join(__dirname, 'config.js')
+    }
+  },
+  plugins: DEBUG ? [
+    new webpack.HotModuleReplacementPlugin()
+  ] : [
+    
+  ]
 }
