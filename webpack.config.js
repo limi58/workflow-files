2cc33b46f26dcb254689f8349ed89f2f176c263c
limi58 <limi58@foxmail.com>
2016-09-02 20:24:58 +0800

    add DefinePlugin

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,10 @@ module.exports = {
   plugins: DEBUG ? [
     new webpack.HotModuleReplacementPlugin()
   ] : [
-    
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': '"production"'
+      }
+    }),
   ]
 }
